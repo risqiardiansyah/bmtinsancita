4c7ec908bbc4462250639356ce0cc6f8e9197193
ruanglabs <ruanglab5@gmail.com>
2020-07-13 08:13:25 +0700

    commit awal

---
+++ database/migrations/2020_07_13_002413_create_pendaftaran_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePendaftaranTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('pendaftaran'))
+        {
+            Schema::dropIfExists('pendaftaran');
+        }
+        Schema::create('pendaftaran', function (Blueprint $table) {
+            $table->char('id',255);
+            $table->string('nomer_induk',255);
+            $table->string('nama_lengkap',255);
+            $table->string('no_ktp',255)->unique();
+            $table->string('no_kk',255)->unique();
+            $table->string('no_whatsapp',255)->unique();
+            $table->string('pekerjaan',255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pendaftaran');
+    }
+}
