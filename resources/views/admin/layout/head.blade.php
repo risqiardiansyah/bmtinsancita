d291b7d9b98d6a3364e2be5c909adb32048a6e1b
ruanglabs <ruanglab5@gmail.com>
2020-07-27 21:57:26 +0700

    sds

--- resources/views/admin/layout/head.blade.php
+++ resources/views/admin/layout/head.blade.php
@@ -14,6 +14,7 @@ $site                 = DB::table('konfigurasi')->first();
   <meta name="viewport" content="width=device-width, initial-scale=1">
  
   <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <!-- Custom fonts for this template -->
   <link href="{{ asset('public/admin/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
